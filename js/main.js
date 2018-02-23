85b00c90460ee0a10413a6d2fe786a6a6c9b6b2d
araditama <arizki.aditama@gmail.com>
2018-02-23 17:28:27 +0700

    gasken

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Home', 'About', 'Product', 'Contact']
+    });
+    $('#moveToTop').click(function(e){
+        e.preventDefault();
+        $.fn.fullpage.moveTo(1, 0);
+    });
+});
